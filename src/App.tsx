432d4f121e57c83b5972fcf000d47e8e72cc9e21
Victor Brito <victor.brito@kumulus.com.br>
2022-02-23 23:13:30 -0300

    feat: messageSender

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import './App.css';
 import * as ct from './chromeServices/content'
 import * as bg from './chromeServices/background'
 
-
 function App() {
   const [str_nmb, setStr_nmb] = React.useState(0)
-  const [pageId, setPageId] = React.useState(0)
-  
-  React.useEffect(() => {
-    chrome.tabs.query({active: true}).then(
-      res1 => res1.length > 0 && res1[0].id && setPageId(res1[0].id)
-    )
-  }, [])
+
+  function messageSender(msg: any) {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+    var activeTab = tabs[0];
+    if (activeTab.id) chrome.tabs.sendMessage(activeTab.id, msg);
+   });
+  } 
 
   async function Increment() {
+    messageSender({'message': 'helloo'})
     const nmb = await chrome.storage?.sync?.get('selected_number') || 0
     const new_nmb = parseInt(nmb?.selected_number) + 1
     if (new_nmb) ct.addNumber(new_nmb)
@@ -28,12 +28,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edite <code>{pageId}</code> and save to reload.
-        </p>
-        <p>
           Button clicked <code>{str_nmb}</code> times.
         </p>
-        <button  style={{cursor: 'pointer'}} onClick={() => Increment()}>Teste</button>
+        <button id="button1" style={{cursor: 'pointer'}} onClick={() => Increment()}>Teste</button>
         <a
           className="App-link"
           href="https://reactjs.org"

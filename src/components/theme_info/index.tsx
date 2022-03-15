6ebb6a614df9e0fb57632ea6aed87a41ccfa3242
Victor Brito <victor.brito@kumulus.com.br>
2022-03-15 02:28:14 -0300

    feat: theme_info icon

--- src/components/theme_info/index.tsx
+++ src/components/theme_info/index.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import * as ts from '../../types'
 import * as sc from './styles'
 
-export default function ThemeInfo(data: ts.ThemeItemType) {
+export default function ThemeInfo({ data }: ts.ThemeItemType) {
     console.log('data received: ',data)
     return(
         <sc.Container>
-            teste
+            <sc.Icon src={'backgrounds/' + data.path + '/thumbnail.png'} alt={data.title} />
+            <p>{data.title}</p>
         </sc.Container>
     )
 }
--- src/components/theme_info/styles.ts
+++ src/components/theme_info/styles.ts
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+    padding: 25px;
+`;
+
+export const Icon = styled.img`
+    max-width: 100%;
+    width: 120px;
 `;

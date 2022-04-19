4f21b3d70fbd3ed35453b927105d4529714b2a34
Victor Brito <victor.brito@kumulus.com.br>
2022-04-19 02:21:36 -0300

    Feat: gif icons

--- src/components/theme_list_item/index.tsx
+++ src/components/theme_list_item/index.tsx
@@ -4,7 +4,7 @@ import * as sc from './styles'
 export default function ThemeListItem({ data, doAction }: ts.ThemeItemType) {
     return(
         <sc.Item onClick={doAction ? doAction : undefined}>
-            <img src={'backgrounds/' + data.path + '/thumbnail.png'} alt={data.title}/>
+            <img src={'backgrounds/' + data.path + '/thumbnail.gif'} alt={data.title}/>
             <p>{data.title}</p>
         </sc.Item>
     )
